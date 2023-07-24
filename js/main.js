9628dd55861c87182ba76373468de5014881a8ff
diana <dskripka005@gmail.com>
2023-07-24 18:47:05 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//1
+let num = 1;
+
+while (num <= 10) {
+  console.log("num: ", num);
+  num += 1;
+};
+
+//2
+for (let i = 2; i <= 20; i += 1) {
+ if (i % 2 === 0) {
+  
+console.log('парні числа>>',i);
+}};
+
+//3
+const increase = 70;
+for (let ii = 7; ii <= increase; ii += 7) {
+    console.log("множення на 7: ", ii);
+}
+
+//4
+
