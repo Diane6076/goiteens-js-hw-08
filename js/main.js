a1bf2d79bf42b01439ad6477da620719b5123b7e
diana <dskripka005@gmail.com>
2023-07-26 18:01:59 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//1
+//*1
 let num = 1;
 
 while (num <= 10) {
@@ -6,18 +6,40 @@ while (num <= 10) {
   num += 1;
 };
 
-//2
+//*2
 for (let i = 2; i <= 20; i += 1) {
  if (i % 2 === 0) {
   
 console.log('парні числа>>',i);
 }};
 
-//3
+//*3
 const increase = 70;
 for (let ii = 7; ii <= increase; ii += 7) {
     console.log("множення на 7: ", ii);
 }
 
-//4
+//*4
 
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log('array:>>', array.length);
+// console.log('array:>>', array[0]);
+// console.log('array:>>', array[7]);
+// console.log('array:>>', array[array.length -1]);
+
+let index = 0;
+
+// while (0 < 10) {}
+while (index < array.length) {
+  index += 1;
+  console.log('el:>>', array[index]);
+};
+
+//*5
+
+
+//*6
+
+
+//*7
